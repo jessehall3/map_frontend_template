7c9a7736e9244640e172e1d16a7cbaf7e64ac860
Jesse Hall <jessehall3@gmail.com>
2019-02-10 18:33:46 -0500

    Now showing a value for kwh if enter one of the sample addresses.

--- main.js
+++ main.js
@@ -5,33 +5,33 @@ function initMap(){
       lng: -82.325008
     },
     zoom: 13
-  });
+  })
 
-  const input = document.getElementById('pac-input');
+  const input = document.getElementById('pac-input')
 
-  const autocomplete = new google.maps.places.Autocomplete(input);
+  const autocomplete = new google.maps.places.Autocomplete(input)
 
-  const infoWindow = new google.maps.InfoWindow();
-  const infoWindowContent = document.getElementById('infoWindow');
-  infoWindow.setContent(infoWindowContent);
+  const infoWindow = new google.maps.InfoWindow()
+  const infoWindowContent = document.getElementById('infoWindow')
+  infoWindow.setContent(infoWindowContent)
   const marker = new google.maps.Marker({
     map: map
-  });
+  })
   marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-  });
+    infoWindow.open(map, marker)
+  })
 
 
   const showLicenseInfo = async function(place) {
-    let myJson
+    let myJson = {}
     const placeId = place.place_id
 
     try{
-      let response = await fetch(_config.db_url+ placeId)
-      myJson = await response.json();
+      let response = await fetch(_config.landlordLicenseUrl + placeId)
+      myJson = await response.json()
     }
     catch(e) {
-      console.log(e);
+      console.log(e)
     }
 
     const key2id = {
@@ -48,32 +48,55 @@ function initMap(){
       const p = document.querySelector("#" + id)
       p.innerText = myJson[key] || not_found
     })
-  };
+  }
+
+  const showElectricInfo = async function(place) {
+    let myJson = {}
+    const placeId = place.place_id
+
+    try{
+      let response = await fetch(_config.electricConsumptionUrl + placeId)
+      myJson = await response.json()
+    }
+    catch(e) {
+      console.log(e)
+    }
+
+    const firstItem = myJson[0]
+    const kwhConsumption = firstItem["kwh_consumption"]
+
+    const not_found = "No Information Found"
+
+    const id = "kwh-consumption"
+    const p = document.querySelector("#" + id)
+    p.innerText = kwhConsumption ? (kwhConsumption + " KWH") : not_found
+  }
 
   autocomplete.addListener('place_changed', function() {
-    infoWindow.close();
+    infoWindow.close()
 
-    const place = autocomplete.getPlace();
-    showLicenseInfo(place);
+    const place = autocomplete.getPlace()
+    showLicenseInfo(place)
+    showElectricInfo(place)
 
     if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
+      map.fitBounds(place.geometry.viewport)
     }
     else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
+      map.setCenter(place.geometry.location)
+      map.setZoom(17)
     }
 
     // Set the position of the marker using the place ID and location.
     marker.setPlace({
       placeId: place.place_id,
       location: place.geometry.location
-    });
-    marker.setVisible(true);
-  });
+    })
+    marker.setVisible(true)
+  })
 }
 
 const jsElem = window.document.createElement('script');
-jsElem.src = `https://maps.googleapis.com/maps/api/js?key=${_config.map_key}&libraries=places&callback=initMap`;
-jsElem.type = 'text/javascript';
+jsElem.src = `https://maps.googleapis.com/maps/api/js?key=${_config.mapKey}&libraries=places&callback=initMap`
+jsElem.type = 'text/javascript'
 document.body.append(jsElem);
